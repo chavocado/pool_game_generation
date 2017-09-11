7757610347d3c2e88748e763c4888da2ce04fd75
chavocado <d.chavez562@yahoo.com>
2017-09-11 10:12:49 -0500

    Client app connected, pool_app controller for logic added, form inputs added. Included radio for seeding type.

---
+++ server/public/scripts/controllers/pool_app.js
@@ -0,0 +1,9 @@
+myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
+  console.log('pool controller running');
+  //$scope.message = "Pool Controller!";
+  //tournament variables
+  $scope.poolNum;
+  $scope.teamNum;
+  $scope.roundNum;
+  $scope.seedType;
+}]);
