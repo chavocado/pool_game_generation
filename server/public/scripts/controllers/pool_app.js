690487b5cf888c1f47374ed6aab7663f4b321ad4
chavocado <d.chavez562@yahoo.com>
2017-09-11 13:36:28 -0500

    begun drafting out logic in my head. thinking of solutions. commented out possible approaches based on requirements

--- server/public/scripts/controllers/pool_app.js
+++ server/public/scripts/controllers/pool_app.js
@@ -6,4 +6,37 @@ myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
   $scope.teamNum;
   $scope.roundNum;
   $scope.seedType;
+  $scope.tournament = {
+    poolNum: $scope.poolNum,
+    teamNum: $scope.teamNum,
+    roundNum: $scope.roundNum,
+    seedType: $scope.seedType,
+    // pools: []
+  };
+
+// Tournament values should be set by the form in the HTML.
+// Onclick function should take tournament object/values and create Pool objects
+// with incremented alphabet naming pattern applied as their name property.
+// one approach would be a pools array that holds pool objects
+//  (this would be easier to display on front end with angular)
+//  these pool objects also need a list of games held in an array
+//  snake seeding for the groups has to occur before the game algorithm runs
+//  game algorithm has to be everyone plays everyone in the same group for 1 game per round
+//  games must have unique Identifiers and 2 teams
+
+//   //empty holding array
+//   var games = [];
+//   //generate pool objects
+//   var pools = new Pool();
+//   //loop through param length
+//   for(var i=0; i<poolNum; i++) {
+//     //increment letter portion
+//     //var letter = 'A';
+//     //String.fromCharCode(letter.charCodeAt() + i)
+//     //
+//     if(arr.indexOf(lowerText)<0) arr.push(lowerText);
+//     else if(duplicate.indexOf(lowerText)<0) duplicate.push(lowerText);
+//   }
+//   return;
+// }
 }]);
