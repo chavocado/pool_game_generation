87474e92c52b06eb287968de1bcbe3b16bb8b4c2
Daniel Chavez <d.chavez562@yahoo.com>
2017-09-11 19:50:33 -0500

    added a module for snake seeding. may remove later when refactoring. Building out logic on front end for now.

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ const path = require('path');
 const app = express();
 
 const index = require('./routes/index');
+const snake = require('./routes/snake');
+const Snake = require('./modules/snake');
+var snake = new Snake(pools, teams, rounds);
 
 // Serve back static files
 app.use(express.static(path.join(__dirname, './public')));
@@ -12,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Routes
+app.use('/snake', snake);
 app.use('/', index);
 
 //port listener
--- server/public/scripts/controllers/pool_app.js
+++ server/public/scripts/controllers/pool_app.js
@@ -1,32 +1,49 @@
 myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
   console.log('pool controller running');
-  //$scope.message = "Pool Controller!";
-  //tournament variables
-  // $scope.poolNum;
-  // $scope.teamNum;
-  // $scope.roundNum;
-  // $scope.seedType;
   $scope.tournamentRules = {
     poolNum: $scope.poolNum,
     teamNum: $scope.teamNum,
     roundNum: $scope.roundNum,
     seedType: $scope.seedType,
-    // pools: []
   };
+  var pools = [];
+  var teams = [];
   
  $scope.submitRules = function(rules){
    console.log('here',$scope.tournamentRules);
+   buildPools();
+   buildTeams();
   }
 
   function buildTeams(){
-    for (var i = 1; i <= teamNum; i++) {
-      $scope.teams.push('Team ' + i);
+    for (var i = 1; i <= $scope.tournamentRules.teamNum; i++) {
+      teams.push('Team ' + i);
     }
-    return $scope.teams;
+    console.log(teams);
+    // return teams;
   }
-
+  
+  function buildPools(){
+    for (var i = 0; i < $scope.tournamentRules.poolNum; i++) {
+    var pool = new Object();
+    pool.name = 'Pool ' +  (String.fromCharCode(65 + i))
+      pools.push(pool);
+      console.log(pools);
+    }
+    console.log('build pools')
+    // return pools;
+  }
+  
+  function snakeSeed(){
+    
+  }
+  
+  function sequenceSeed(){
+    
+  }
 // Tournament values should be set by the form in the HTML.
-// Onclick function should take tournament object/values and create Pool objects
+// Onclick function should take tournament object/values and create Pool objec
+// one poold ts
 // with incremented alphabet naming pattern applied as their name property.
 // one approach would be a pools array that holds pool objects
 //  (this would be easier to display on front end with angular)
@@ -35,21 +52,6 @@ myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
 //  game algorithm has to be everyone plays everyone in the same group for 1 game per round
 //  games must have unique Identifiers and 2 teams
 
-//   //empty holding array
-//   var games = [];
-//   //generate pool objects
-//   var pools = new Pool();
-//   //loop through param length
-//   for(var i=0; i<poolNum; i++) {
-//     //increment letter portion
-//     //var letter = 'A';
-//     //String.fromCharCode(letter.charCodeAt() + i)
-//     //
-//     if(arr.indexOf(lowerText)<0) arr.push(lowerText);
-//     else if(duplicate.indexOf(lowerText)<0) duplicate.push(lowerText);
-//   }
-//   return;
-// }
 
 
 }]);
