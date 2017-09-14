68a281a4d5e91a4c7678b4af51a3482fcb9d67ac
Daniel Chavez <d.chavez562@yahoo.com>
2017-09-14 18:49:49 -0500

    fiixed issues with messages

--- server/public/scripts/controllers/pool_app.js
+++ server/public/scripts/controllers/pool_app.js
@@ -13,44 +13,43 @@ myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
   //let pools = [];
   //let teams = [];
 
- $scope.submitRules = function(data) {
-   console.log('here', $scope.tournamentRules);
-   let request = '?pools=' + data.poolNum + '&teams=' + data.teamNum +
-                 '&rounds=' + data.roundNum + '&seed=' + data.seedType;
-  //  $http.get('/tournamentBuild/' + request)
-  //     .then(function (response) {
-  //       console.log(response);
-  //       if(response.status == 400 || response.status == 500){
-  //         alert("response.data.errors[0]");
-  //       }
-  //       $scope.pools = response.data;
-   //
-  //       for(var i = 0; i < $scope.pools.length; i++ ) {
-  //           $scope.games.push($scope.pools[i].games);
-  //       }
-   //
-  //       console.log(response.data[0], response.data[1]);
-  //       console.log('GET /tournamentBuild', response.data);
-  //       console.log($scope.games);
-  //     });
+ // $scope.submitRules = function(data) {
+ //   console.log('here', $scope.tournamentRules);
+ //   let request = '?pools=' + data.poolNum + '&teams=' + data.teamNum +
+ //                 '&rounds=' + data.roundNum + '&seed=' + data.seedType;
+ //   $http.get('/tournamentBuild/' + request)
+ //      .then(function (response) {
+ //        console.log(response);
+ //        if(response.status == 400 || response.status == 500){
+ //          alert("response.data.errors[0]");
+ //        }
+ //        $scope.pools = response.data;
+ //   
+ //        for(var i = 0; i < $scope.pools.length; i++ ) {
+ //            $scope.games.push($scope.pools[i].games);
+ //        }
+ //   
+ //        console.log(response.data[0], response.data[1]);
+ //        console.log('GET /tournamentBuild', response.data);
+ //        console.log($scope.games);
+ //      });
+ //  };
 
-      $scope.submitRules = function(data) {
-        console.log('here', $scope.tournamentRules);
-        let request = '?pools=' + data.poolNum + '&teams=' + data.teamNum +
-                      '&rounds=' + data.roundNum + '&seed=' + data.seedType;
-        $http.get('/tournamentBuild/' + request)
-           .then(function successCallback(response) {
-             $scope.pools = response.data;
-             for(var i = 0; i < $scope.pools.length; i++ ) {
-                 $scope.games.push($scope.pools[i].games);
-             }
-             console.log('GET /tournamentBuild', response.data);
-           }, function errorCallback(response) {
-              if(response.status == 400 || response.status == 500){
-                alert("response.data.errors[0]");
-              }
-           });
-
-  };
-
+  $scope.submitRules = function(data) {
+    console.log('here', $scope.tournamentRules);
+    let request = '?pools=' + data.poolNum + '&teams=' + data.teamNum +
+                  '&rounds=' + data.roundNum + '&seed=' + data.seedType;
+    $http.get('/tournamentBuild/' + request)
+       .then(function successCallback(response) {
+         $scope.pools = response.data;
+         for(var i = 0; i < $scope.pools.length; i++ ) {
+             $scope.games.push($scope.pools[i].games);
+         }
+         console.log('GET /tournamentBuild', response.data);
+       }, function errorCallback(response) {
+          if(response.status == 400 || response.status == 500){
+            alert(response.data.errors[0]);
+          }
+       });
+   }; 
 }]);
