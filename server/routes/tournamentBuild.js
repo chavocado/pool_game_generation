7aab1b554978e2fed98ab9939e301e24fc5c51ef
Daniel Chavez <d.chavez562@yahoo.com>
2017-09-13 22:14:08 -0500

    testing and messages working properly

--- server/routes/tournamentBuild.js
+++ server/routes/tournamentBuild.js
@@ -8,7 +8,11 @@ router.get('/', function (req, res) {
   rules.pools = Number(rules.pools);
   rules.teams = Number(rules.teams);
   rules.rounds = Number(rules.rounds);
+  
   console.log('This Happened', req.query);
+  if (rules.seed === undefined || (rules.seed !== 'snake' && rules.seed !== 'sequential')){
+    errors.push('seed must be a string of "sequential" or "snake"')
+  }
   if (isNaN(rules.pools) || rules.pools === undefined) {
     errors.push('pools must be a number')
   }
@@ -18,26 +22,15 @@ router.get('/', function (req, res) {
   if (isNaN(rules.teams) || rules.teams === undefined) {
     errors.push('teams must be a number')
   }
-  if ((rules.seed !== 'sequential') && (rules.seed !== 'snake') || (rules.seed === undefined)){
-    errors.push('seed must be a string of "sequential" or "snake]"')
-  }
+  
   if(errors.length === 0){
     res.send(Tournament(rules));
   } else {
-    res.send({
+    res.status(400).send({
       errors : errors
-    }).status(400)
+    })
   }
-  //if (err) {
-  //  res.sendStatus(500);
-  //}
-  // function (err, result){
-  // if (err) {
-  //   res.sendStatus(500);
-  //   return;
-  // }
-  //   res.sendStatus(201);
-  // });
+
   res.send(Tournament(req.query));
 });
 
