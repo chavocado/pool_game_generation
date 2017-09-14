1f0fa3ed732a57a1918cb1e3d442ce5c255b561c
chavocado <d.chavez562@yahoo.com>
2017-09-14 16:36:31 -0500

    changing alerts

--- server/public/scripts/controllers/pool_app.js
+++ server/public/scripts/controllers/pool_app.js
@@ -17,31 +17,39 @@ myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
    console.log('here', $scope.tournamentRules);
    let request = '?pools=' + data.poolNum + '&teams=' + data.teamNum +
                  '&rounds=' + data.roundNum + '&seed=' + data.seedType;
-   $http.get('/tournamentBuild/' + request)
-      .then(function (response) {
-        console.log(response);
-        if(response.status == 400 || response.status == 500){
-          alert("response.data.errors[0]");
-        }
-        $scope.pools = response.data;
+  //  $http.get('/tournamentBuild/' + request)
+  //     .then(function (response) {
+  //       console.log(response);
+  //       if(response.status == 400 || response.status == 500){
+  //         alert("response.data.errors[0]");
+  //       }
+  //       $scope.pools = response.data;
+   //
+  //       for(var i = 0; i < $scope.pools.length; i++ ) {
+  //           $scope.games.push($scope.pools[i].games);
+  //       }
+   //
+  //       console.log(response.data[0], response.data[1]);
+  //       console.log('GET /tournamentBuild', response.data);
+  //       console.log($scope.games);
+  //     });
 
-        for(var i = 0; i < $scope.pools.length; i++ ) {
-            $scope.games.push($scope.pools[i].games);
-        }
-
-        console.log(response.data[0], response.data[1]);
-        console.log('GET /tournamentBuild', response.data);
-        console.log($scope.games);
-      });
-  //  $http({
-  //    method: 'GET',
-  //    url: '/tournamentBuild',
-  //    data: data
-  //  }).then(function(){
-  //    console.log('success');
-  //  },function() {
-  //    console.log('failure');
-  //  })
+      $scope.submitRules = function(data) {
+        console.log('here', $scope.tournamentRules);
+        let request = '?pools=' + data.poolNum + '&teams=' + data.teamNum +
+                      '&rounds=' + data.roundNum + '&seed=' + data.seedType;
+        $http.get('/tournamentBuild/' + request)
+           .then(function successCallback(response) {
+             $scope.pools = response.data;
+             for(var i = 0; i < $scope.pools.length; i++ ) {
+                 $scope.games.push($scope.pools[i].games);
+             }
+             console.log('GET /tournamentBuild', response.data);
+           }, function errorCallback(response) {
+              if(response.status == 400 || response.status == 500){
+                alert("response.data.errors[0]");
+              }
+           });
 
   }
 
