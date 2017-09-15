b5b5718b2b83f2521cef6b7c60a2487a3ef7e312
Daniel Chavez <d.chavez562@yahoo.com>
2017-09-14 20:22:50 -0500

    added sweet alerts for errors. reordered error messages to make more sense on th ui.

--- server/public/scripts/controllers/pool_app.js
+++ server/public/scripts/controllers/pool_app.js
@@ -1,42 +1,18 @@
 myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
   console.log('pool controller running');
+  //global scope vars
+  $scope.tournamentRules.seedType = 'snake'
+  $scope.pools = [];
+  $scope.games = [];
   $scope.tournamentRules = {
     poolNum: $scope.poolNum,
     teamNum: $scope.teamNum,
     roundNum: $scope.roundNum,
     seedType: $scope.seedType,
   };
-  $scope.tournamentRules.seedType = 'snake'
-  $scope.pools = [];
-  // $scope.pools.teams
-  $scope.games = [];
-  //let pools = [];
-  //let teams = [];
-
- // $scope.submitRules = function(data) {
- //   console.log('here', $scope.tournamentRules);
- //   let request = '?pools=' + data.poolNum + '&teams=' + data.teamNum +
- //                 '&rounds=' + data.roundNum + '&seed=' + data.seedType;
- //   $http.get('/tournamentBuild/' + request)
- //      .then(function (response) {
- //        console.log(response);
- //        if(response.status == 400 || response.status == 500){
- //          alert("response.data.errors[0]");
- //        }
- //        $scope.pools = response.data;
- //   
- //        for(var i = 0; i < $scope.pools.length; i++ ) {
- //            $scope.games.push($scope.pools[i].games);
- //        }
- //   
- //        console.log(response.data[0], response.data[1]);
- //        console.log('GET /tournamentBuild', response.data);
- //        console.log($scope.games);
- //      });
- //  };
-
+  
   $scope.submitRules = function(data) {
-    console.log('here', $scope.tournamentRules);
+    //console.log('here', $scope.tournamentRules);
     let request = '?pools=' + data.poolNum + '&teams=' + data.teamNum +
                   '&rounds=' + data.roundNum + '&seed=' + data.seedType;
     $http.get('/tournamentBuild/' + request)
@@ -45,10 +21,12 @@ myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
          for(var i = 0; i < $scope.pools.length; i++ ) {
              $scope.games.push($scope.pools[i].games);
          }
-         console.log('GET /tournamentBuild', response.data);
+         //console.log('GET /tournamentBuild', response.data);
        }, function errorCallback(response) {
           if(response.status == 400 || response.status == 500){
-            alert(response.data.errors[0]);
+            swal('Oops...',
+                 '' + response.data.errors[0],
+                 'warning');
           }
        });
    }; 
