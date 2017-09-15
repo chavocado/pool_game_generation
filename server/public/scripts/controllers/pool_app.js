3e2831e4f49db586e477327750c0b4b97eacd2e1
Daniel Chavez <d.chavez562@yahoo.com>
2017-09-14 20:28:29 -0500

    rearranging globals

--- server/public/scripts/controllers/pool_app.js
+++ server/public/scripts/controllers/pool_app.js
@@ -1,18 +1,17 @@
 myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
   console.log('pool controller running');
-  //global scope vars
-  $scope.tournamentRules.seedType = 'snake'
-  $scope.pools = [];
-  $scope.games = [];
   $scope.tournamentRules = {
     poolNum: $scope.poolNum,
     teamNum: $scope.teamNum,
     roundNum: $scope.roundNum,
     seedType: $scope.seedType,
   };
-  
+  $scope.tournamentRules.seedType = 'snake'
+  $scope.pools = [];
+  $scope.games = [];
+
   $scope.submitRules = function(data) {
-    //console.log('here', $scope.tournamentRules);
+    console.log('here', $scope.tournamentRules);
     let request = '?pools=' + data.poolNum + '&teams=' + data.teamNum +
                   '&rounds=' + data.roundNum + '&seed=' + data.seedType;
     $http.get('/tournamentBuild/' + request)
@@ -21,7 +20,7 @@ myApp.controller('PoolController', ['$scope','$http',function($scope,$http) {
          for(var i = 0; i < $scope.pools.length; i++ ) {
              $scope.games.push($scope.pools[i].games);
          }
-         //console.log('GET /tournamentBuild', response.data);
+         console.log('GET /tournamentBuild', response.data);
        }, function errorCallback(response) {
           if(response.status == 400 || response.status == 500){
             swal('Oops...',
