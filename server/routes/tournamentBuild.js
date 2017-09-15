68007de241d147892124cc2b3113c44ca6f8417d
Daniel Chavez <d.chavez562@yahoo.com>
2017-09-14 20:58:12 -0500

    visual changes

--- server/routes/tournamentBuild.js
+++ server/routes/tournamentBuild.js
@@ -26,7 +26,8 @@ router.get('/', function (req, res) {
     errors.push('rounds must be a number greater than or equal to 1')
   }
   if (rules.seed == 'snake' && rules.pools === 1){
-    errors.push('pools must be a number greater than or equal to 2 when selecting snake seeding')
+    errors.push('pools must be a number greater than or equal to 2 ' +
+                'when selecting snake seeding. Please use sequential seeding')
   }
 
   if(errors.length === 0){
